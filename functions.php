cfc17244db5e062e57421502cec3904b5ea6498e
Jujujulie44 <julieandre2008@live.fr>
2024-01-09 12:18:52 +0100

    correction hook

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), time()); 
 
+     // Enqueue le script Swiper
+
+    wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@6.8.4/swiper-bundle.min.js', array('jquery'), '6.8.4', true);
+    // Enqueue le script du thème enfant
+    wp_enqueue_script('script', get_theme_file_uri('/js/script.js'), array('jquery', 'swiper'), '1.0.0', true);
+
 }
 
 // Get customizer options form parent theme
@@ -15,24 +21,4 @@ if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
-
- // Enqueue le script Swiper
-
- wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@6.8.4/swiper-bundle.min.js', array('jquery'), '6.8.4', true);
- // Enqueue le script du thème enfant
- wp_enqueue_script('script', get_theme_file_uri('/js/script.js'), array('jquery', 'swiper'), '1.0.0', true);
-
 }
-
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-// Synchronisation des modifications entre le thème enfant et le thème parent
-if (get_stylesheet() !== get_template()) {
-    add_filter('pre_update_option_theme_mods_' . get_stylesheet(), function ($valeur, $ancienne_valeur) {
-        update_option('theme_mods_' . get_template(), $valeur);
-        return $ancienne_valeur;
-    }, 10, 2);
-    add_filter('pre_option_theme_mods_' . get_stylesheet(), function ($par_defaut) {
-        return get_option('theme_mods_' . get_template(), $par_defaut);
-    });
-}
-?>
